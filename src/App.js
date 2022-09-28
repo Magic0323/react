56bb4d9ed6246cea69939c06d00abbb3a1f68b50
Magic0323 <magic_yw2001@163.com>
2022-09-28 17:08:13 +0800

    antd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { DownloadOutlined } from '@ant-design/icons';
+import { Button, Radio } from 'antd';
+import React, { useState } from 'react';
 
 function App() {
+  let size = 'large'
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Button type="primary" size={size}>
+        Primary
+      </Button>
+      <Button size={size}>Default</Button>
+      <Button type="dashed" size={size}>
+        Dashed
+      </Button>
+    </>
+
   );
 }
 
